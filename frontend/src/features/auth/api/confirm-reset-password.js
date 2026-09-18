f86d89a59b801df4ee3944fe991aa52cd4675e70
ella perry <craniumdonuts@gmail.com>
2026-09-18 20:24:53 +1200

    Remove unused confirm-reset-password files (firebase handles reset)

--- frontend/src/features/auth/api/confirm-reset-password.js
+++
@@ -1,6 +0,0 @@
-import { auth } from "../../../shared/auth";
-import { confirmPasswordReset } from "firebase/auth";
-
-export const confirmResetPassword = async ({ oobCode, newPassword }) => {
-    await confirmPasswordReset(auth, oobCode, newPassword);
-};
