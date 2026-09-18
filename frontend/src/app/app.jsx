cec648594dc7b7014b47f2847303939b19868807
ella perry <craniumdonuts@gmail.com>
2026-09-18 16:58:56 +1200

    TasksProvider into App

--- frontend/src/app/app.jsx
+++ frontend/src/app/app.jsx
@@ -1,14 +1,17 @@
 import { Provider } from "./provider";
 import { Router } from "./router";
 import { GlobalReminders } from "./GlobalReminders";
+import { TasksProvider } from "../features/tasks/context/TasksContext";
 
 function App() {
-  return (
-    <Provider>
-      <Router />
-      <GlobalReminders />
-    </Provider>
-  );
+    return (
+        <Provider>
+            <TasksProvider>
+                <GlobalReminders />
+                <Router />
+            </TasksProvider>
+        </Provider>
+    );
 }
 
 export { App };
